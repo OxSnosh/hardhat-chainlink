f64d324917893f7bf6d8560ff16250d9afb06386
OxSnosh <OxSnosh@proton.me>
2023-05-06 15:03:55 -0500

    updated deploy oracle task to use Operator.sol

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require('@nomicfoundation/hardhat-toolbox')
+require('hardhat-deploy')
 // require('./dist')
 
 const commonCompilerSettings = {
@@ -33,5 +34,12 @@ module.exports = {
         settings: commonCompilerSettings,
       }
     ]
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
   }
 }
+
--- src/tasks/deploy-oracle.ts
+++ src/tasks/deploy-oracle.ts
@@ -1,22 +1,24 @@
 import { ActionType } from "hardhat/types";
 
-import OracleArtifact from "../../artifacts/@chainlink/contracts/src/v0.4/Oracle.sol/Oracle.json";
+// import OracleArtifact from "../../artifacts/@chainlink/contracts/src/v0.4/Oracle.sol/Oracle.json";
 
 export const deployOracle: ActionType<{
   nodeAddress: string;
   linkAddress: string;
 }> = async (taskArgs, hre) => {
   const { nodeAddress, linkAddress } = taskArgs;
+  const { getNamedAccounts } = require("hre");
+  const deployer = await getNamedAccounts().deployer
 
-  const Oracle = await hre.ethers.getContractFactoryFromArtifact(
-    OracleArtifact
+  const Oracle = await hre.ethers.getContractFactory(
+    "Operator"
   );
-  const oracle = await Oracle.deploy(linkAddress);
+  const oracle = await Oracle.deploy(linkAddress, deployer);
 
   await oracle.deployed();
 
   // Set Fulfillment on Oracle
-  await oracle.setFulfillmentPermission(nodeAddress, true);
+  await oracle.setAutorizedSenders([nodeAddress])
 
   console.log(
     "All set on this end! If you've setup everything correctly, you can start getting external data from your smart contract"
